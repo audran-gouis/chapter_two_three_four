07e292461fa7d3b42f8e0f4f766d1ca799d8c337
Audran Gouis <audran.gouis@gmail.com>
2024-11-11 14:36:37 +0100

    Adding the two functions for defining the two first properties of the Product struct

--- riamol_two_three_four/src/main.rs
+++ riamol_two_three_four/src/main.rs
@@ -1,24 +1,25 @@
 use rand::Rng;
 use std::fmt;
+use std::io;
 
 #[derive(Debug)]
-struct warehouse {
+struct Warehouse {
     id : u64,
     items_in_stock : u32,
     location : String,
-    products : Option<Vec<Products>>,
-    repair_products : Option<Vec<Products>>,
-    digital_products : Option<Vec<Products>>,
-    house_products : Option<Vec<Products>>,
+    products : Option<Vec<Product>>,
+    repair_products : Option<Vec<Product>>,
+    digital_products : Option<Vec<Product>>,
+    house_products : Option<Vec<Product>>,
 }
 
-impl fmt::Display for warehouse {
+impl fmt::Display for Warehouse {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
     }
 }
 
-impl warehouse {
+impl Warehouse {
   
     fn create_warehouse() -> Self {
         let random_15 : u64;
@@ -56,23 +57,59 @@ enum Categories {
     House,
 }
 #[derive(Debug)]
-struct Products {
+struct Product {
     product_id : u32,
     name : String,
-    price : f32, 
-    category : Categories,
-    warehouse_id : u32,
-    stocks : u32,
-    discount : u32,
-    country_made_in : String,
+    price : Option<u32>, 
+    category : Option<Categories>,
+    warehouse_id : Option<u32>,
+    stocks : Option<u32>,
+    discount : Option<u32>,
+    country_made_in : Option<String>,
 }
-// function for generating a random number for product ID
+
+impl fmt::Display for Product {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 // function to put items in a vector (the one of the warehouse)
 // function to apply discount based on category
 // function to put the item in the warehouse that is located the closest from its country where it has been made
 
+impl Product {
+    fn new_product() -> Self {
+        
+        println!("Please input a product name.");
+        let mut name = String::new();
+        io::stdin()
+            .read_line(&mut name)
+            .expect("Failed to read line");
+
+        fn generate_random_6_digit_number() -> u32 {
+            let mut rng = rand::thread_rng();
+            rng.gen_range(100_000..1_000_000)
+        }
+
+            let random_6 = generate_random_6_digit_number();
+
+            Self {
+                product_id : random_6,
+                name : name,
+                price : None,
+                category : None, 
+                warehouse_id : None, 
+                stocks : None, 
+                discount : None, 
+                country_made_in : None, 
+            }
+        }
+    }
 
 fn main () {
-    let mut warehouse_one = warehouse::create_warehouse();
+    let mut warehouse_one = Warehouse::create_warehouse();
     println!("{}", warehouse_one);
+    let mut product_one = Product::new_product();
+    println!("{}", product_one);
 }
